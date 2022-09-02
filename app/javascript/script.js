cd66df5b740c734a86d090b18fdc33b98632909e
EC2 Default User <ec2-user@ip-172-31-27-16.ap-northeast-1.compute.internal>
2022-09-02 13:46:44 +0000

    [Update]jQuery動作確認

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,2 +1,8 @@
 let title = "javascriptが使えました。";
 alert(title);
+
+/*global $*/$(document).ready(function(){
+    $('.jquery').on('click',function(){
+        $(this).css('color','red');
+    });
+});
