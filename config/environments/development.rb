f93ef67ab8cfa10e2455cfe197d2b9f455a3eaf5
EC2 Default User <ec2-user@ip-172-31-27-16.ap-northeast-1.compute.internal>
2022-09-02 13:39:35 +0000

    [Add]jQuery追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "a84a5a61d87742199b5d27d77df01b0e.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
